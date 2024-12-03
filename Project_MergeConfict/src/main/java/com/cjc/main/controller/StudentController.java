afef49b707c99d0043474d5a4542d75a8530dc53
vishalmorey1998 <vishalmorey2000@gmail.com>
2024-12-03 11:38:44 +0530

    Update StudentController.java
    By Dev11

--- Project_MergeConfict/src/main/java/com/cjc/main/controller/StudentController.java
+++ Project_MergeConfict/src/main/java/com/cjc/main/controller/StudentController.java
@@ -4,6 +4,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+@Autowired StudentService se;
+	@postMapping("/saveStudentData)
+	public ResponseEntity<Student> onSaveData(Student int id)
+	{
+            
+	Student	stud=se.onSaveStudentData(id);
+		Return new ResponseEntity<Student>(stud,HttpStatus.Created);
+			
+	}
+
+		
 
 	
 
